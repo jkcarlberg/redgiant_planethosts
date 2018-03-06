b31abc06440753ed7ebea753b85d37993c000e4a
Doug Branton <dbranton@stsci.edu>
2018-03-06 16:36:10 -0500

    Dump results into JSON file

--- tame-1.1.0/auto_tame.py
+++ tame-1.1.0/auto_tame.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python
 
 import numpy as np
-from astropy.io import fits
-import os
-import csv
 import pandas as pd
 import pidly
 import glob
+import json
 
 
 if __name__ == "__main__":
@@ -15,14 +13,17 @@ if __name__ == "__main__":
     lowercut_space = np.linspace(0.95, 0.995, 10)
 
     for spectrum in spectra:
+
         spec_label = spectrum.split('_wavsoln')[0]
         print(spec_label)
+        lowercut_dict = {}
+
         for lowercut in lowercut_space:
 
             # Read in Parameter File
             df = pd.read_csv('tame.par', delim_whitespace=True, header=None)
 
-            # Modify Parameter File (Lowercut)
+            # Modify Parameter File (Lowercut & Working File)
             df.at[df.index[df[0] == "LOWERCUT"].tolist()[0], 1] = lowercut
             df.at[df.index[df[0] == "WORKNAME"].tolist()[0], 1] = spec_label
             df.at[df.index[df[0] == "SPECTRUM"].tolist()[0], 1] = spec_label+'.lpx'
@@ -36,6 +37,14 @@ if __name__ == "__main__":
             idl = pidly.IDL('/Applications/exelis/idl/bin/idl')
             idl('tame_silent')
 
-        # Read Output File
+            # Read Output File
+            tame_ew = pd.read_csv("{}_auto.aout".format(spec_label), skiprows=1,
+                                  delim_whitespace=True, header=None)
+            tame_df = tame_ew[[0, 4]]  # Wavelength and Equivalent Width
+            lowercut_dict[lowercut] = np.array(tame_df).tolist()
+
+        print(lowercut_dict)
+        with open('{}.json'.format(spec_label), 'w') as fp:
+            json.dump(lowercut_dict, fp)
 
 
