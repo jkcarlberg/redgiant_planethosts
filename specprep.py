ec99e12632accc33ffd4d883ad3f70e7945a41e0
Doug Branton <dbranton@stsci.edu>
2018-03-07 14:51:49 -0500

    Bulk prep spectra with measured EWs

--- specprep.py
+++ specprep.py
@@ -5,20 +5,26 @@ from astropy.io import fits
 import os
 import csv
 import pandas as pd
+import glob
 
 
 class SpecPrep:
 
-    def __init__(self, filename, path='.', outdir='.', gen_ew_file=True):
-        self.filename = filename
+    def __init__(self, files, path='.', outdir='tame-1.1.0/', gen_ew_file=True):
+        self.files = files
         self.path = path
         self.outdir = outdir
         self.gen_ew_file = gen_ew_file
         self.cluster_list_corr = "cluster_fe_lines.csv"
         self.cluster_list_orig = "cluster_fe_lines_orig.csv"
-        self.onedspec_fits(self.filename, path=self.path, outdir=self.outdir)
-        self.spec_lpx(self.filename, path=self.outdir, outdir=self.outdir)
-        self.linelist_from_csv(self.filename, outdir=self.outdir, gen_ew_file=self.gen_ew_file)
+        self.file_glob = glob.glob(os.path.join(path, self.files))
+        for filename in self.file_glob:
+            print(filename)
+            has_measurement = self.linelist_from_csv(filename, outdir=self.outdir, gen_ew_file=self.gen_ew_file)
+            if has_measurement:
+                self.onedspec_fits(filename, path=self.path, outdir=self.outdir)
+                self.spec_lpx(filename, path=self.outdir, outdir=self.outdir)
+
 
     @staticmethod
     def onedspec_fits(filename, path='.', outdir='.'):
@@ -55,12 +61,15 @@ class SpecPrep:
             wavelcol = fits.Column(name='WAVEL', format='E', array=wave)
             wav_hdu = fits.BinTableHDU.from_columns([fluxcol, wavelcol], name="WAV")
             hdulist.append(wav_hdu)
+
+            filename = filename.split('/')[-1]
             hdulist.writeto(os.path.join(outdir, filename.split('.')[0]+"_wavsoln.fits"),
                             overwrite=True)
 
     @staticmethod
     def spec_lpx(filename, path='.', outdir='.'):
         """Generate a spectrum text file for use with TAME"""
+        filename = filename.split('/')[-1]
         with fits.open(os.path.join(path, filename.split('.')[0]+"_wavsoln.fits")) as hdulist:
             wavelength = hdulist['WAV'].data['WAVEL']
             flux = hdulist['WAV'].data['FLUX']
@@ -73,18 +82,24 @@ class SpecPrep:
         """Generate a linelist for a file from the cluster csv list"""
         df_corr = pd.read_csv(self.cluster_list_corr)
         df_orig = pd.read_csv(self.cluster_list_orig, skiprows=1)
+        print(filename)
+        filename = filename.split('/')[-1]
         star_num = filename.split('_')[1][:-3]  # Isolate star number and remove 'red'
+        print(star_num)
         columns = ['Wavelength', 'Ion', 'eP', 'Log-gf', star_num]
+        has_measurement = False
 
         if star_num in df_corr.keys():
             print("Found Star in Redone Spreadsheet")
             df = df_corr[columns]
+            has_measurement = True
         elif star_num in df_orig.keys():
             print("Found Star in Original Spreadsheet")
             df = df_orig[columns]
+            has_measurement = True
         else:
             print("No Equivalent Width measurements taken for Star. Exiting.")
-            return
+            return has_measurement
 
         # Mask skipped wavelengths
         m = np.isnan(df[star_num]) == False
@@ -100,6 +115,8 @@ class SpecPrep:
             with open(os.path.join(outdir, filename.split('.')[0]+'.ew'), 'w') as f:
                 writer = csv.writer(f, delimiter=' ')
                 writer.writerows(masked_ew)
+
+        return has_measurement
 
 
 if __name__ == "__main__":
