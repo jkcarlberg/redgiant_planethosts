75947730e461b199091617134e1640f27c9ac013
Doug Branton <dbranton@stsci.edu>
2018-05-25 16:03:30 -0400

    updated auto_tame.py for non-byhand

--- tame-1.1.0/auto_tame.py
+++ tame-1.1.0/auto_tame.py
@@ -9,13 +9,16 @@ import json
 
 if __name__ == "__main__":
 
-    spectra = glob.glob("*wavsoln.fits")
+    input_dir = "../data/ph_ctrl_stars/tame_inputs/"
+    output_dir = "../data/ph_ctrl_stars/tame_outputs/"
+
+    spectra = glob.glob(input_dir+ "*wavsoln.fits")
     lowercut_space = np.linspace(0.95, 0.995, 10)
     idl = pidly.IDL('/Applications/exelis/idl/bin/idl')
 
     for spectrum in spectra:
 
-        spec_label = spectrum.split('_wavsoln')[0]
+        spec_label = spectrum.split('/')[-1].split('_wavsoln')[0]
         print(spec_label)
         line_dict = {}
         for lowercut in lowercut_space:
@@ -25,10 +28,11 @@ if __name__ == "__main__":
 
             # Modify Parameter File (Lowercut & Working File)
             df.at[df.index[df[0] == "LOWERCUT"].tolist()[0], 1] = lowercut
-            df.at[df.index[df[0] == "WORKNAME"].tolist()[0], 1] = spec_label
-            df.at[df.index[df[0] == "SPECTRUM"].tolist()[0], 1] = spec_label+'.lpx'
-            df.at[df.index[df[0] == "LINELIST"].tolist()[0], 1] = spec_label+'.lines'
-            df.at[df.index[df[0] == "OUTPUT"].tolist()[0], 1] = spec_label+'_auto_sm1.aout'
+            df.at[df.index[df[0] == "WORKNAME"].tolist()[0], 1] = input_dir + spec_label
+            df.at[df.index[df[0] == "SPECTRUM"].tolist()[0], 1] = input_dir + spec_label+'.lpx'
+            #df.at[df.index[df[0] == "LINELIST"].tolist()[0], 1] = spec_label+'.lines'
+            df.at[df.index[df[0] == "LINELIST"].tolist()[0], 1] = input_dir + 'input_lines.lines'
+            df.at[df.index[df[0] == "OUTPUT"].tolist()[0], 1] = output_dir + spec_label + '_auto.aout'
             df.at[df.index[df[0] == "SMOOTHER"].tolist()[0], 1] = "1"
 
             # Write new Parameter File
@@ -38,17 +42,17 @@ if __name__ == "__main__":
             idl('tame_silent')
 
             # Read Output File
-            tame_ew = pd.read_csv("{}_auto_sm1.aout".format(spec_label), skiprows=1,
+            tame_ew = pd.read_csv("{}{}_auto.aout".format(output_dir,spec_label), skiprows=1,
                                   delim_whitespace=True, header=None)
-            tame_df = tame_ew[[0, 4]]  # Wavelength and Equivalent Width
-            for line, ew in np.array(tame_df).tolist():
+            tame_df = tame_ew[[0, 4, 6]]  # Wavelength and Equivalent Width
+            for line, ew, linecent in np.array(tame_df).tolist():
                 if line not in line_dict:
-                    line_dict[line] = [(lowercut, ew)]
+                    line_dict[line] = [(lowercut, ew, linecent)]
                 else:
-                    line_dict[line].append((lowercut, ew))
+                    line_dict[line].append((lowercut, ew, linecent))
 
         print(line_dict)
-        with open('{}_sm1.json'.format(spec_label), 'w') as fp:
+        with open('{}{}.json'.format(output_dir, spec_label), 'w') as fp:
             json.dump(line_dict, fp)
 
 
